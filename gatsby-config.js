63e167a95541c7983a17db2ddebce229abc326ad
Benoit <dev@benoitmaigret.com>
2018-01-23 11:21:45 +1100

    Add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  plugins: [`gatsby-plugin-styled-components`]
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-64656539-1',
+        anonymize: true
+      }
+    }
+  ]
 }
