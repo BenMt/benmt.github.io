12a0a4454c9489ba39612833cb84ae948bd16c19
Benoit <dev@benoitmaigret.com>
2018-01-23 11:24:55 +1100

    Add TranslatorProvider in SSR

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,20 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
- // You can delete this file if you're not using it
+import React from 'react'
+import { TranslatorProvider } from 'react-translate'
+import { renderToString } from 'react-dom/server'
+
+import fr from './src/utils/intl/fr'
+import en from './src/utils/intl/en'
+
+exports.replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
+  const translations = { fr, en }
+  const language = 'en'
+  const TranslatedBody = () => (
+    <TranslatorProvider translations={translations[language]}>
+      {bodyComponent}
+    </TranslatorProvider>
+  )
+  replaceBodyHTMLString(renderToString(<TranslatedBody />))
+}
