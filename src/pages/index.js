9810abb584c9089c7c0678b08bf3fd87a5ae5fa8
Benoit <dev@benoitmaigret.com>
2018-01-27 15:16:01 +1100

    fr page is the default page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,83 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { withPrefix } from 'gatsby-link'
 
-class RedirectIndex extends React.PureComponent {
+import IndexPage from '../templates/IndexPage'
+
+class IndexFrPage extends React.Component {
   constructor(args) {
     super(args)
 
     // Skip build, Browsers only
     if (typeof window !== 'undefined') {
-      const langKey = 'en'
-      const homeUrl = withPrefix(`/${langKey}/`)
-
-      // I don`t think this is the best solution
-      // I would like to use Gatsby Redirects like:
-      // https://github.com/gatsbyjs/gatsby/tree/master/examples/using-redirects
-      // But Gatsby Redirects are static, they need to be specified at build time,
-      // This redirect is dynamic, It needs to know the user browser language.
-      // Any ideias? Join the issue: https://github.com/angeloocana/gatsby-starter-default-i18n/issues/4
-      window.___history.replace(homeUrl)
+      if (navigator.language !== 'fr') {
+        const homeUrl = withPrefix(`/en/`)
+        window.___history.replace(homeUrl)
+      }
     }
   }
 
   render() {
-    return <div />
+    const { data } = this.props
+    return (
+      <IndexPage
+        author={data.site.siteMetadata.author}
+        work={data.site.siteMetadata.work}
+        contact={data.site.siteMetadata.contact}
+        translations={data.site.siteMetadata.fr}
+        lang="fr"
+      />
+    )
   }
 }
 
-export default RedirectIndex
+IndexFrPage.propTypes = {
+  data: PropTypes.shape({}).isRequired
+}
+
+export default IndexFrPage
+export const query = graphql`
+  query IndexFrQuery {
+    site {
+      siteMetadata {
+        author {
+          name
+        }
+        contact {
+          type
+          value
+          link
+        }
+        work {
+          title
+          link
+          image
+          skills
+        }
+        fr {
+          navigation {
+            title
+            href
+          }
+          job {
+            short
+            long
+          }
+          location
+          work
+          contact
+          about {
+            description
+            now
+            sydney
+            sideproject
+            skills
+            other
+            junior
+          }
+          cec
+        }
+      }
+    }
+  }
+`
