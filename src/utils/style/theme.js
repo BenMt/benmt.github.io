1c7783dc918688ab8b34b9875b9b0aed04d7cec1
Benoit Maigret <dev@benoitmaigret.com>
2018-08-23 11:30:02 +0200

    Updates

--- src/utils/style/theme.js
+++
@@ -1,9 +0,0 @@
-const theme = {
-  color: { main: '#f56d45', mainHover: '#fd8866' },
-  font: {
-    main: "'Open Sans', sans-serif",
-    secondary: ' Raleway, Open Sans, Arial, sans-serif'
-  }
-}
-
-export default theme
