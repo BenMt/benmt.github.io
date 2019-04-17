e77fad990dceb176149477a6ca414491e219f348
Benoit Maigret <dev@benoitmaigret.com>
2019-04-17 10:26:40 +0200

    Use graphql in Header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
 import styled, { withTheme } from 'styled-components'
 import { Link } from 'gatsby'
 import { Box, Flex, Text } from 'rebass'
@@ -36,72 +37,114 @@ const GatsbyLink = styled(Link)`
   }
 `
 
-const Footer = ({ theme, mainNavigation }) => (
-  <Box bg={theme.color.dark}>
-    <Wrapper>
-      <Flex
-        flexDirection={['column', 'row']}
-        justifyContent="space-between"
-        alignItems="center"
-        py="0.2rem"
-      >
-        <Flex
-          flex="1"
-          color="#fff"
-          alignItems="center"
-          justifyContent="flex-start"
-        >
-          {mainNavigation.map(navElement => (
-            <GatsbyLink key={navElement.id} to={navElement.url} role="link">
-              {navElement.title}
-            </GatsbyLink>
-          ))}
-        </Flex>
-        <Flex
-          flex="1"
-          justifyContent="center"
-          alignItems="center"
-          m={['1rem 0', '0']}
-        >
-          <Text color="#fff" fontSize="0.7rem">
-            <CodeLink
-              href="https://github.com/BenMt/benmt.github.io"
-              target="_BLANK"
-              rel="noopener noreferrer"
+const Footer = ({ theme, langKey }) => (
+  <StaticQuery
+    query={graphql`
+      query Footer {
+        en: allContentfulSettings(filter: { node_locale: { eq: "en" } }) {
+          edges {
+            node {
+              title
+              job
+              source
+              mainNavigation {
+                id
+                title
+                url
+              }
+            }
+          }
+        }
+        fr: allContentfulSettings(filter: { node_locale: { eq: "fr" } }) {
+          edges {
+            node {
+              title
+              job
+              source
+              mainNavigation {
+                id
+                title
+                url
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Box bg={theme.color.dark}>
+        <Wrapper>
+          <Flex
+            flexDirection={['column', 'row']}
+            justifyContent="space-between"
+            alignItems="center"
+            py="0.2rem"
+          >
+            <Flex
+              flex="1"
+              color="#fff"
+              alignItems="center"
+              justifyContent="flex-start"
             >
-              Source available on Github
-            </CodeLink>
-          </Text>
-        </Flex>
-        <Flex alignItems="center" flex="1" justifyContent="flex-end">
-          <SocialLink
-            href="https://github.com/BenMt"
-            target="_BLANK"
-            role="link"
-            aria-label="Github"
-          >
-            <FaGithub />
-          </SocialLink>
-          <SocialLink
-            href="https://twitter.com/benoitmaigret"
-            target="_BLANK"
-            role="link"
-            aria-label="Twitter"
-          >
-            <FaTwitter />
-          </SocialLink>
-          <SocialLink
-            href="https://www.linkedin.com/in/benoitmaigret/"
-            target="_BLANK"
-            role="link"
-            aria-label="LnkedIn"
-          >
-            <FaLinkedin />
-          </SocialLink>
-        </Flex>
-      </Flex>
-    </Wrapper>
-  </Box>
+              {data[langKey].edges[0].node.mainNavigation.map(navElement => (
+                <GatsbyLink key={navElement.id} to={navElement.url} role="link">
+                  {navElement.title}
+                </GatsbyLink>
+              ))}
+            </Flex>
+            <Flex
+              flex="1"
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+              m={['1rem 0', '0']}
+            >
+              <Text color="#fff" fontSize="0.7rem">
+                {`${data[langKey].edges[0].node.title} | ${
+                  data[langKey].edges[0].node.job
+                }`}
+              </Text>
+              <Text color="#fff" fontSize="0.7rem">
+                <CodeLink
+                  href="https://github.com/BenMt/benmt.github.io"
+                  target="_BLANK"
+                  rel="noopener noreferrer"
+                >
+                  {data[langKey].edges[0].node.source}
+                </CodeLink>
+              </Text>
+            </Flex>
+            <Flex alignItems="center" flex="1" justifyContent="flex-end">
+              <SocialLink
+                href="https://github.com/BenMt"
+                target="_BLANK"
+                role="link"
+                aria-label="Github"
+              >
+                <FaGithub />
+              </SocialLink>
+              <SocialLink
+                href="https://twitter.com/benoitmaigret"
+                target="_BLANK"
+                role="link"
+                aria-label="Twitter"
+              >
+                <FaTwitter />
+              </SocialLink>
+              <SocialLink
+                href="https://www.linkedin.com/in/benoitmaigret/"
+                target="_BLANK"
+                role="link"
+                aria-label="LnkedIn"
+              >
+                <FaLinkedin />
+              </SocialLink>
+            </Flex>
+          </Flex>
+        </Wrapper>
+      </Box>
+    )}
+  />
 )
 
 export default withTheme(Footer)
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,100 @@
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components'
 import { Link } from 'gatsby'
-import { Flex } from 'rebass'
+import { Flex, Text } from 'rebass'
 
 const Container = styled.header`
   width: 100%;
 `
 
-const StyledLink = styled(Link)`
-  color: #fff;
-  text-decoration: ${props => (props.isCurrent ? 'underline' : 'none')};
-  padding: 1rem;
+const StyledLink = styled.div`
+  & > a {
+    display: block;
+    padding: 1rem;
+    color: #fff;
+    text-decoration: ${props => (props.isCurrent ? 'underline' : 'none')};
 
-  &:hover {
-    color: ${props => props.theme.color.main};
+    &:hover {
+      color: ${props => props.theme.color.main};
+    }
   }
 `
 
-const Header = ({ currentPathname, mainNavigation }) => (
-  <Container>
-    <Flex
-      role="navigation"
-      color="#fff"
-      alignItems="flex-end"
-      justifyContent="center"
-    >
-      {mainNavigation.map(navElement => (
-        <StyledLink
-          key={navElement.id}
-          to={navElement.url}
-          isCurrent={currentPathname === navElement.url}
-          role="link"
+const Header = ({ currentPathname, langKey }) => (
+  <StaticQuery
+    query={graphql`
+      query Header {
+        en: allContentfulSettings(filter: { node_locale: { eq: "en" } }) {
+          edges {
+            node {
+              title
+              job
+              source
+              mainNavigation {
+                id
+                title
+                url
+              }
+            }
+          }
+        }
+        fr: allContentfulSettings(filter: { node_locale: { eq: "fr" } }) {
+          edges {
+            node {
+              title
+              job
+              source
+              mainNavigation {
+                id
+                title
+                url
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Container>
+        <Flex
+          role="navigation"
+          color="#fff"
+          alignItems="flex-end"
+          justifyContent="center"
         >
-          {navElement.title}
-        </StyledLink>
-      ))}
-    </Flex>
-  </Container>
+          {data[langKey].edges[0].node.mainNavigation.map(navElement => (
+            <StyledLink
+              key={navElement.id}
+              isCurrent={currentPathname === navElement.url}
+            >
+              <Link to={navElement.url} role="link">
+                {navElement.title}
+              </Link>
+            </StyledLink>
+          ))}
+          <Flex css={{ position: 'absolute', top: 0, right: 0 }}>
+            <StyledLink>
+              <Link
+                to={
+                  (currentPathname === '/parcours' ? '/en/resume' : '') ||
+                  (currentPathname === '/en/resume' && '/parcours') ||
+                  (langKey === 'fr'
+                    ? `en/${currentPathname}`
+                    : currentPathname.replace('en/', '/'))
+                }
+                role="link"
+              >
+                <Text fontSize="0.8rem" css={{ lineHeight: '1.65rem' }}>
+                  {langKey === 'fr' ? 'English version' : 'Version française'}
+                </Text>
+              </Link>
+            </StyledLink>
+          </Flex>
+        </Flex>
+      </Container>
+    )}
+  />
 )
 
 export default Header
